fa411fdbb8264b13a1911927e587e45bc2abcfe8
Nathanael <nathanaeltimotius3@gmail.com>
2024-04-03 16:56:49 +0700

    modified redis

--- main.go
+++ main.go
@@ -1,29 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"week9/controllers"
+	"week9/models"
 )
 
 func main() {
 	// config sementara untuk bisa send email
-	// config := models.NewEmailConfig(
-	// 	"smtp.gmail.com",
-	// 	587,
-	// 	"irvand9999@gmail.com",
-	// 	"ggha yggy gogy lmti",
-	// )
+	config := models.NewEmailConfig(
+		"smtp.gmail.com",
+		587,
+		"irvand9999@gmail.com",
+		"ggha yggy gogy lmti",
+	)
 
-	// //Ini bagian yang bisa dimodifikasi (line 20-22)
-	// recipientEmail := "irvand9999@gmail.com"
-	// subject := "Test Go Mail doang"
-	// body := "Hello <b>Irvan</b> ini adalah sebuah test email dari Gomail"
+	//Ini bagian yang bisa dimodifikasi (line 20-22)
+	recipientEmail := "irvand9999@gmail.com"
+	subject := "Test Go Mail doang"
+	body := "Hello <b>Irvan</b> ini adalah sebuah test email dari Gomail"
 
-	// if err := controllers.SendEmail(config, recipientEmail, subject, body); err != nil {
-	// 	fmt.Println("Gagal mengirim email:", err)
-	// 	return
-	// }
+	if err := controllers.SendEmail(config, recipientEmail, subject, body); err != nil {
+		fmt.Println("Gagal mengirim email:", err)
+		return
+	}
 
-	// fmt.Println("Email berhasil dikirim!")
+	fmt.Println("Email berhasil dikirim!")
 
 	// testing redis
 	controllers.RedisClient()
